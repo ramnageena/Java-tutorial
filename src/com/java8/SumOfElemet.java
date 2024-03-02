a59695b79109b1fbfe75e8d3cfa36ca0aefde396
Ram Tiwari <ramtiwari8716@gmail.com>
2024-03-03 00:52:47 +0530

    commit 5 : Program based on Java 8

--- src/com/java8/SumOfElemet.java
+++ src/com/java8/SumOfElemet.java
@@ -8,8 +8,14 @@ public class SumOfElemet {
     public static void main(String[] args) {
         List<Integer> num = List.of(1, 2, 3, 4, 15,5);
         int sum = num.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(sum);
+        System.out.println("Sum of all Element :"+sum);
 
-
+        //IInd ways
+        int sum1 = num.stream().mapToInt(i -> i).sum();
+        System.out.println("IInd ways Sum of all Element :"+sum1);
     }
 }
+/*
+Sum of all Element :30
+IInd ways Sum of all Element :30
+ */
