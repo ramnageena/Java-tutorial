8cc754b1e56763cf51f02173085aad7f4246e85e
Ram Tiwari <ramtiwari8716@gmail.com>
2024-04-06 19:28:44 +0530

    commit 8 :Constructor in java

---
+++ src/com/oops/Constructor/Student.java
@@ -0,0 +1,28 @@
+package com.oops.Constructor;
+
+/*
+   Constructor :- Constructor name should be same as Class name,
+    Constructor are used to analysis the instance of object.
+
+    ------------Type of Constructor---------
+    1-Default constructor
+    2-Parameter Constructor
+
+ */
+public class Student {
+    int id;
+    String name;
+    String city;
+
+    Student(int id, String name, String city){
+        System.out.println("Id :"+ " " + id + " Name :" +" "+ name + " City:" +" "+ city);
+    }
+    Student(int id, String name){
+        System.out.println("Id :"+ " " + id + " Name :" +" "+ name );
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student(1,"Ram","Chennai");
+        Student s1 = new Student(1,"Ram");
+    }
+}
