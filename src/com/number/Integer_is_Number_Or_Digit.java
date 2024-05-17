32fe8ac552a0d47561fccd6b79c0e6e55f159ded
Ram Tiwari <ramtiwari8716@gmail.com>
2024-05-17 17:15:42 +0530

    commit 13 : Coding question inside number

--- src/com/number/Integer_is_Number_Or_Digit.java
+++ src/com/number/Integer_is_Number_Or_Digit.java
@@ -8,7 +8,14 @@ public class Integer_is_Number_Or_Digit {
         System.out.println("Enter the Integer value");
         int in = s.nextInt();
 
+        // 1 way
         if(in/10==0){
+            System.out.println("The Integer value is Digit");
+        }else
+            System.out.println("The Integer value is Number");
+
+        // 2 ways
+        if(in<10 && in>-10){
             System.out.println("The Integer value is Digit");
         }else
             System.out.println("The Integer value is Number");
